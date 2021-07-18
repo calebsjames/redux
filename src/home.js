76734713613f0a6348a3cf3861686ba36e3d4a74
Caleb James <calebsjames@gmail.com>
2021-07-18 13:25:32 -0500

    build: basic form and buttons

--- src/home.js
+++ src/home.js
@@ -1,8 +1,30 @@
 import React from "react";
 
+const addTen = () => {
+    
+}
+const reduceTen = () => {
+
+}
+
 const Home = () => {
     return (
+    <>
     <h1>Hello</h1>
+    <article id="form">
+        <form className="promoterForm">
+            <h2 className="formTitle">Balance</h2>
+                <div className="flex">
+                    <fieldset className="form">
+                        <div className="form-group">
+                            <button onClick={addTen}>+10</button>
+                            <button onClick={reduceTen}>-10</button>
+                        </div>
+                    </fieldset>
+                </div>
+        </form>
+    </article>
+    </>
     )
 }
 
